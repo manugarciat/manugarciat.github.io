4e17d67bae9da61b6161ddf177a9c445aca7b253
Manuel García Tornadú <manuelgarciatornadu@gmail.com>
2024-05-11 19:57:41 -0300

    primer editado b

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ function App() {
 
     return (
         <>
-            <h2><code>manugarciat.github.io</code></h2>
+            <div className="logo">
+                <h2><code>manugarciat.github.io</code></h2>
+            </div>
             <h4>work in progress</h4>
             <div className="card">
-              <p className="gris">
-                <code> Manuel García Tornadú / Portfolio en construcción.</code>
-              </p>
-
+                <p className="gris">
+                    <code> Manuel García Tornadú / Portfolio en construcción.</code>
+                </p>
             </div>
         </>
     )
